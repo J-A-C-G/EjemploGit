e13b6bd0d0a5c99e18d0f4de552282cdafe7c11e
jaime <ja-carrerag@hotmail.com>
2017-04-14 13:19:30 -0500

    Toast example

--- app/src/main/java/com/example/ejemplogit/MainActivity.java
+++ app/src/main/java/com/example/ejemplogit/MainActivity.java
@@ -3,16 +3,36 @@ package com.example.ejemplogit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    //declaramos la variable imageView de tipo ImageView
+    Button button;
+    Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*//acceder a la imagen por medio del id
+        imageView=(ImageView)findViewById(R.id.imageView);
+        //si queremos modificar atributos ponemos
+        imageView.setColorFilter(454);
+*/
+        button=(Button)findViewById(R.id.boton);
+        button.setOnClickListener(this);
     }
 
-    public void Vista(View view){
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.boton:
+                Toast.makeText(getApplicationContext(), "Hola esto es un toast", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(getApplicationContext(),"Esto es por efecto", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
