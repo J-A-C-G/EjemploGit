c06dda93b68b1b703d168823f1ec42ad2a472092
jaime <ja-carrerag@hotmail.com>
2017-04-14 21:54:58 -0500

    ComunicacionDosActivities

--- app/src/main/java/com/example/ejemplogit/MainActivity.java
+++ app/src/main/java/com/example/ejemplogit/MainActivity.java
@@ -1,47 +1,38 @@
 package com.example.ejemplogit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     //declaramos las variables
+    private Button btn;
+    private EditText txt;
 
-    TextView tV;
-    ImageView iV;
-    Button bT, bI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    //vamos a enlazarlos con sus respectivos ids
-
-        tV=(TextView)findViewById(R.id.tV);
-        iV=(ImageView)findViewById(R.id.iV);
-        bT=(Button)findViewById(R.id.bT);
-        bI=(Button)findViewById(R.id.bI);
-
-    //Necesario para activar el action listener
-        bT.setOnClickListener(this);
-        bI.setOnClickListener(this);
+        btn=(Button)findViewById(R.id.btn_activity);
+        btn.setOnClickListener(this);
+        txt=(EditText)findViewById(R.id.txtNombre);
     }
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.bT:
-                tV.setText("Chao Jaime");
+
+        switch (v.getId()){
+            case R.id.btn_activity:
+                Intent intent= new Intent(this, Main2Activity.class);
+                String vrbDato= txt.getText().toString();
+                intent.putExtra("VRBDATO",vrbDato);
+                startActivity(intent);
                 break;
-            case R.id.bI:
-                iV.setImageResource(R.drawable.icono_android);
+            default:
                 break;
         }
     }
