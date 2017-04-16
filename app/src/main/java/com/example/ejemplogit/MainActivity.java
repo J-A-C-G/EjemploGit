9bbdf9fcf2dadeff8c24fc1063e92f2c4dd8c46d
jaime <ja-carrerag@hotmail.com>
2017-04-15 20:59:17 -0500

    ComunicacionDosActivities

--- app/src/main/java/com/example/ejemplogit/MainActivity.java
+++ app/src/main/java/com/example/ejemplogit/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.ejemplogit;
 
 import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     //declaramos las variables
     private Button btn;
     private EditText txt;
+    private RadioGroup radioGroup;
 
 
     @Override
@@ -20,6 +25,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn=(Button)findViewById(R.id.btn_activity);
         btn.setOnClickListener(this);
         txt=(EditText)findViewById(R.id.txtNombre);
+        //buscar el id del radiogroup
+        radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
+        //llamar al metodo onchecked
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+
+                if(checkedId==R.id.opcion1){
+                    Toast.makeText(getApplicationContext(),"1",Toast.LENGTH_SHORT).show();
+                }else if(checkedId==R.id.opcion2) {
+                    Toast.makeText(getApplicationContext(), "2", Toast.LENGTH_SHORT).show();
+                }
+                else if(checkedId==R.id.opcion3){
+                    Toast.makeText(getApplicationContext(), "3", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
     @Override
